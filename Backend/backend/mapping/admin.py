41753be68ace9dcc02a01c948154707b5e511af4
hamid nawaz <hamid.nawaz28@yahoo.com>
2020-09-29 02:10:36 -0700

    Invoice data columns display

--- Backend/backend/mapping/admin.py
+++ Backend/backend/mapping/admin.py
@@ -4,8 +4,8 @@ class TempleteAdmin(admin.ModelAdmin):
       list_display = ['templeteName','templeteItems']
 class SavedTempleteAdmin(admin.ModelAdmin):
       list_display = ['SavedTempleteName','SavedTempleteColumns','defaultTempleteReference']
-# class AllColumnsAdmin(admin.ModelAdmin):
-#       list_display = ['templeteName','templeteItems']
+class InvoiceColumnsAdmin(admin.ModelAdmin):
+      list_display =     ["INVOICE_ID","GL_DATE","INV_ORGINE","INV_DATE","INV_SOURCE","INV_NUMBER","VENDOR_NUMBER","VENDOR_NAME","VENDOR_TYPE","INV_TERMS","LINE_NUMBER","LINE_TYPE","LINE_DESCRIPTION","LINE_QUANTITY","LINE_UNIT_PRICE","LINE_UNIT_OF_MEASURE","LINE_AMOUNT","GENERAL_LEDGER","LOCATION","DEPARTMENT","ACCOUNT","PO_NUMBER","CREATION_DATE","AllTempletesPK","SavedTempletesPK"]
 admin.site.register(AllTempletes,TempleteAdmin)
 admin.site.register(SavedTempletes,SavedTempleteAdmin)
-admin.site.register(invoiceColumns)
+admin.site.register(invoiceColumns,InvoiceColumnsAdmin)
